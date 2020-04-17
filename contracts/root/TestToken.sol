54d59b9b7907b27cee912937a342c2151bfbf453
arthcp <arth@matic.network>
2020-04-17 12:45:21 +0530

    chg: rename ParentToken and mint while deploying

--- contracts/root/TestToken.sol
+++ contracts/root/TestToken.sol
@@ -2,12 +2,14 @@ pragma solidity "0.6.6";
 
 import { ERC20 } from "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
-contract ParentToken is ERC20 {
+contract TestToken is ERC20 {
   constructor(
     string memory name,
     string memory symbol,
     uint8 decimals
   ) public ERC20(name, symbol) {
     _setupDecimals(decimals);
+    uint256 value = 10**10 * (10**18);
+    _mint(msg.sender, value);
   }
 }
