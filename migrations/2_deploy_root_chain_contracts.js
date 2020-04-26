edac00b842f04ca14e7fcf9651e03db229bd6910
arthcp <arth@matic.network>
2020-04-27 05:09:45 +0530

    chg: link required libraries in migrations

--- migrations/2_deploy_root_chain_contracts.js
+++ migrations/2_deploy_root_chain_contracts.js
@@ -1,9 +1,38 @@
+const bluebird = require('bluebird')
+
+const Merkle = artifacts.require('Merkle')
+const MerklePatriciaProof = artifacts.require('MerklePatriciaProof')
+const RLPReader = artifacts.require('RLPReader')
+
 const RootChainManager = artifacts.require('RootChainManager')
 const DummyStateSender = artifacts.require('DummyStateSender')
 const DummyToken = artifacts.require('DummyToken')
+
 const utils = require('./utils')
 
+const libDeps = [
+  {
+    lib: Merkle,
+    contracts: [RootChainManager]
+  },
+  {
+    lib: MerklePatriciaProof,
+    contracts: [RootChainManager]
+  },
+  {
+    lib: RLPReader,
+    contracts: [RootChainManager]
+  }
+]
+
 module.exports = async(deployer) => {
+  console.log('linking libs...')
+  await bluebird.map(libDeps, async e => {
+    await deployer.deploy(e.lib)
+    deployer.link(e.lib, e.contracts)
+  })
+
+  console.log('deploying contracts...')
   await deployer.deploy(RootChainManager)
   await deployer.deploy(DummyStateSender)
   await deployer.deploy(DummyToken)
