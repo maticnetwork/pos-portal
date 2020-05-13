a994504733a672cb48635a0ef5227c0c64668a14
arthcp <arth@matic.network>
2020-05-13 15:22:23 +0530

    new: contracts for Network Agnostic tokens

---
+++ contracts/child/ChildNADummy.sol
@@ -0,0 +1,19 @@
+pragma solidity >=0.4.21 <0.7.0;
+
+import { ChildToken } from "./ChildToken.sol";
+import { EIP712MetaTransaction } from "../common/EIP712.sol";
+
+contract ChildNADummy is ChildToken, EIP712MetaTransaction {
+  constructor()
+    ChildToken("ChildNADummy", "NADUMMY", 18)
+    EIP712MetaTransaction("ChildNADummy", "1")
+    public
+  {
+    uint256 initialSupply = 10**10 * (10**18);
+    _mint(_msgSender(), initialSupply);
+  }
+
+  function mint(uint256 supply) public {
+    _mint(_msgSender(), supply);
+  }
+}
