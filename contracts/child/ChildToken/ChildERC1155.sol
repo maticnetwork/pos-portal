f77895e79658c9b9cf3d4f794de1261bf6123f39
arthcp <arth@matic.network>
2020-06-15 20:05:26 +0530

    new: Child ERC1155

---
+++ contracts/child/ChildToken/ChildERC1155.sol
@@ -0,0 +1,84 @@
+pragma solidity "0.6.6";
+
+import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
+import {IChildToken} from "./IChildToken.sol";
+
+// import {NetworkAgnostic} from "../../common/NetworkAgnostic.sol";
+
+// TODO: network agnostic
+contract ChildERC20 is ERC1155, IChildToken, AccessControl {
+    bytes32 public constant DEPOSITOR_ROLE = keccak256("DEPOSITOR_ROLE");
+
+    address private _rootToken;
+
+    constructor(string memory uri) public ERC1155(uri) {
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+        _setupRole(DEPOSITOR_ROLE, _msgSender());
+    }
+
+    modifier only(bytes32 role) {
+        require(
+            hasRole(role, _msgSender()),
+            "ChildERC1155: INSUFFICIENT_PERMISSIONS"
+        );
+        _;
+    }
+
+    function setRootToken(address newRootToken)
+        external
+        only(DEFAULT_ADMIN_ROLE)
+    {
+        _rootToken = newRootToken;
+    }
+
+    function rootToken() public view returns (address) {
+        return _rootToken;
+    }
+
+    function _msgSender()
+        internal
+        override
+        view
+        returns (address payable sender)
+    {
+        if (msg.sender == address(this)) {
+            bytes memory array = msg.data;
+            uint256 index = msg.data.length;
+            assembly {
+                // Load the 32 bytes word from memory with the address on the lower 20 bytes, and mask those.
+                sender := and(
+                    mload(add(array, index)),
+                    0xffffffffffffffffffffffffffffffffffffffff
+                )
+            }
+        } else {
+            sender = msg.sender;
+        }
+        return sender;
+    }
+
+    function deposit(address user, bytes calldata depositData)
+        external
+        override
+        only(DEPOSITOR_ROLE)
+    {
+        (
+            uint256[] memory ids,
+            uint256[] memory amounts,
+            bytes memory data
+        ) = abi.decode(depositData, (uint256[], uint256[], bytes));
+        require(user != address(0x0), "ChildERC1155: INVALID_DEPOSIT_USER");
+        _mintBatch(user, ids, amounts, data);
+    }
+
+    function withdrawSingle(uint256 id, uint256 amount) external {
+        _burn(_msgSender(), id, amount);
+    }
+
+    function withdrawBatch(uint256[] calldata ids, uint256[] calldata amounts)
+        external
+    {
+        _burnBatch(_msgSender(), ids, amounts);
+    }
+}
