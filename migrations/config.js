0c31af804c5a6aaa03fb4378e84ca1e46ffefa07
arthcp <arth@matic.network>
2020-06-15 12:37:37 +0530

    chg: update config and package.json

--- migrations/config.js
+++ migrations/config.js
@@ -1,4 +1,5 @@
 module.exports = {
-  plasmaRootChain: '0x82a72315E16cE224f28E1F1fB97856d3bF83f010',
+  // plasmaRootChain: '0x82a72315E16cE224f28E1F1fB97856d3bF83f010', // testnetv3
+  plasmaRootChain: '0x2890bA17EfE978480615e330ecB65333b880928e', // mumbai
   stateReceiver: '0x0000000000000000000000000000000000001001'
 }
