d7ad40a14aa3d9275f74b3a4ac497f39e5099e78
arthcp <arth@matic.network>
2020-06-15 18:45:10 +0530

    chg: variable changes in predicates for readability

--- contracts/root/TokenPredicates/ERC721Predicate.sol
+++ contracts/root/TokenPredicates/ERC721Predicate.sol
@@ -11,8 +11,8 @@ contract ERC721Predicate is ITokenPredicate {
     bytes32 public constant TRANSFER_EVENT_SIG = 0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef;
 
     event LockedERC721(
-        address indexed from,
-        address indexed user,
+        address indexed depositor,
+        address indexed depositReceiver,
         address indexed rootToken,
         uint256 tokenId
     );
@@ -28,7 +28,7 @@ contract ERC721Predicate is ITokenPredicate {
         emit LockedERC721(depositor, depositReceiver, rootToken, tokenId);
     }
 
-    function validateExitLog(address msgSender, bytes calldata log)
+    function validateExitLog(address withdrawer, bytes calldata log)
         external
         override
         pure
@@ -37,27 +37,27 @@ contract ERC721Predicate is ITokenPredicate {
         RLPReader.RLPItem[] memory logTopicRLPList = logRLPList[1].toList(); // topics
         require(
             bytes32(logTopicRLPList[0].toUint()) == TRANSFER_EVENT_SIG, // topic0 is event sig
-            "RootChainManager: INVALID_SIGNATURE"
+            "ERC721Predicate: INVALID_SIGNATURE"
         );
         require(
-            msgSender == address(logTopicRLPList[1].toUint()), // topic1 is from address
-            "RootChainManager: INVALID_SENDER"
+            withdrawer == address(logTopicRLPList[1].toUint()), // topic1 is from address
+            "ERC721Predicate: INVALID_SENDER"
         );
         require(
             address(logTopicRLPList[2].toUint()) == address(0), // topic2 is to address
-            "RootChainManager: INVALID_RECEIVER"
+            "ERC721Predicate: INVALID_RECEIVER"
         );
     }
 
     function exitTokens(
-        address msgSender,
+        address withdrawer,
         address rootToken,
         bytes memory log
     ) public override {
         RLPReader.RLPItem[] memory logRLPList = log.toRlpItem().toList();
         IERC721(rootToken).transferFrom(
             address(this),
-            msgSender,
+            withdrawer,
             logRLPList[2].toUint() // log data field
         );
     }
