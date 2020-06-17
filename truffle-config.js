cacf87386c5036c48a6ebc93baf9d4b5530111ee
Jaynti Kanani <jdkanani@gmail.com>
2020-06-17 16:39:27 +0530

    fix: change networks and add gas price plugin for tests

--- truffle-config.js
+++ truffle-config.js
@@ -25,7 +25,10 @@ const HDWalletProvider = require('@truffle/hdwallet-provider')
 //
 // const fs = require('fs');
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
-const mnemonic = 'clock radar mass judge dismiss just intact mind resemble fringe diary casino'
+
+const MNEMONIC = 'clock radar mass judge dismiss just intact mind resemble fringe diary casino'
+const API_KEY = process.env.API_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 
 module.exports = {
   /**
@@ -39,49 +42,37 @@ module.exports = {
    */
 
   networks: {
-    root: {
-      // host: 'localhost',
-      // port: 9545,
-      // network_id: '53227'
-
-      // provider: () =>
-      //   new HDWalletProvider(
-      //     mnemonic,
-      //     'https://ropsten.infura.io/v3/70645f042c3a409599c60f96f6dd9fbc'
-      //   ),
-      // network_id: 3,
-      // gas: 7000000,
-      // gasPrice: 10000000000, // 10 gwei
-      // skipDryRun: true
-
+    development: {
+      host: 'localhost',
+      port: 9545,
+      network_id: '*', // match any network
+      skipDryRun: true,
+      gas: 7000000
+    },
+    bor: {
       provider: () =>
         new HDWalletProvider(
-          mnemonic,
-          'https://goerli.infura.io/v3/70645f042c3a409599c60f96f6dd9fbc'
+          MNEMONIC,
+          'http://localhost:8545'
+        ),
+      network_id: '*', // match any network
+      gasPrice: '0'
+    },
+    mumbaiRoot: {
+      provider: () =>
+        new HDWalletProvider(
+          MNEMONIC,
+          `https://goerli.infura.io/v3/${API_KEY}`
         ),
       network_id: 5,
       gas: 7000000,
       gasPrice: 10000000000, // 10 gwei
       skipDryRun: true
     },
-    child: {
-      // host: 'localhost',
-      // port: 8545,
-      // network_id: '96410'
-
-      // provider: () =>
-      //   new HDWalletProvider(
-      //     mnemonic,
-      //     'https://testnetv3.matic.network'
-      //   ),
-      // network_id: 15001,
-      // gas: 7000000,
-      // gasPrice: 10000000000, // 10 gwei
-      // skipDryRun: true
-
+    mumbaiBor: {
       provider: () =>
         new HDWalletProvider(
-          mnemonic,
+          MNEMONIC,
           'https://rpc-mumbai.matic.today'
         ),
       network_id: 80001,
@@ -93,21 +84,36 @@ module.exports = {
 
   // Set default mocha options here, use special reporters etc.
   mocha: {
-    // timeout: 100000
+    reporter: 'eth-gas-reporter',
+    reporterOptions: {
+      currency: 'USD',
+      gasPrice: 21,
+      outputFile: '/dev/null',
+      showTimeSpent: true
+    }
   },
 
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.6.6",    // Fetch exact version from solc-bin (default: truffle's version)
-      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //  optimizer: {
-      //    enabled: false,
-      //    runs: 200
-      //  },
-      //  evmVersion: "byzantium"
-      // }
+      version: '0.6.6', // Fetch exact version from solc-bin (default: truffle's version)
+      docker: true, // Use "0.5.1" you've installed locally with docker (default: false)
+      parser: 'solcjs',
+      settings: { // See the solidity docs for advice about optimization and evmVersion
+        // optimizer: {
+        //   enabled: false,
+        //   runs: 200
+        // }
+        // evmVersion: 'constantinople'
+      }
     }
+  },
+
+  verify: {
+    preamble: 'Matic PoS Portal'
+  },
+
+  api_keys: {
+    etherscan: ETHERSCAN_API_KEY
   }
 }
