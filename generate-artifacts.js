97ac5736f8c432c94ab78e98322364e2cdae02ab
arthcp <arth@matic.network>
2020-06-26 13:13:23 +0530

    chg: change script to copy and generate required artifacts

---
+++ generate-artifacts.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+
+const artifactsToGenerate = [
+  'ChildChainManager.json',
+  'ChildERC1155.json',
+  'ChildERC20.json',
+  'ChildERC721.json',
+  'DummyERC1155.json',
+  'DummyERC20.json',
+  'DummyERC721.json',
+  'MaticWETH.json',
+  'RootChainManager.json'
+]
+
+artifactsToGenerate.forEach(a => {
+  const source = `build/contracts/${a}`
+  const dest = `artifacts/${a}`
+  const abi = JSON.parse(fs.readFileSync(source)).abi
+  fs.writeFileSync(dest, JSON.stringify({ abi }) + '\n')
+})
