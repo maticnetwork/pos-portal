95ed58c7b6c49984f447fb0724f03cb79d131c7a
arthcp <arth@matic.network>
2020-07-01 01:45:59 +0530

    new: ERC721Predicate tests

--- test/helpers/logs.js
+++ test/helpers/logs.js
@@ -1,7 +1,8 @@
 import { RLP } from 'ethers/utils'
 
 import {
-  erc20TransferEventSig
+  erc20TransferEventSig,
+  erc721TransferEventSig
 } from './constants'
 
 export const getERC20TransferLog = ({
@@ -18,5 +19,22 @@ export const getERC20TransferLog = ({
       to
     ],
     '0x' + amount.toString(16)
+  ])
+}
+
+export const getERC721TransferLog = ({
+  overrideSig,
+  from,
+  to,
+  tokenId
+}) => {
+  return RLP.encode([
+    '0x0',
+    [
+      overrideSig || erc721TransferEventSig,
+      from,
+      to,
+      '0x' + tokenId.toString(16)
+    ]
   ])
 }
