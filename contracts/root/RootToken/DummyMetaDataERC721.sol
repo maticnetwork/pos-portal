60334190d5de66fb6b1052152feac294bcf74d4b
arthcp <arth@matic.network>
2020-08-04 11:19:54 +0530

    chg: access control and comments

--- contracts/root/RootToken/DummyMetaDataERC721.sol
+++ contracts/root/RootToken/DummyMetaDataERC721.sol
@@ -5,15 +5,26 @@ import {NetworkAgnostic} from "../../common/NetworkAgnostic.sol";
 import {ChainConstants} from "../../ChainConstants.sol";
 import {ContextMixin} from "../../common/ContextMixin.sol";
 import {IMetaDataERC721} from "./IMetaDataERC721.sol";
+import {AccessControlMixin} from "../../common/AccessControlMixin.sol";
 
-// todo: comments
-// todo: access control
-contract DummyMetaDataERC721 is ERC721, NetworkAgnostic, ChainConstants, ContextMixin, IMetaDataERC721 {
+contract DummyMetaDataERC721 is
+    ERC721,
+    NetworkAgnostic,
+    ChainConstants,
+    ContextMixin,
+    IMetaDataERC721,
+    AccessControlMixin
+{
+    bytes32 public constant PREDICATE_ROLE = keccak256("PREDICATE_ROLE");
     constructor(string memory name_, string memory symbol_)
         public
         ERC721(name_, symbol_)
         NetworkAgnostic(name_, ERC712_VERSION, ROOT_CHAIN_ID)
-    {}
+    {
+        _setupContractId("ChildMetaDataERC721");
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+        _setupRole(PREDICATE_ROLE, _msgSender());
+    }
 
     function _msgSender()
         internal
@@ -24,19 +35,30 @@ contract DummyMetaDataERC721 is ERC721, NetworkAgnostic, ChainConstants, Context
         return ContextMixin.msgSender();
     }
 
-    function mint(uint256 tokenId) public {
+    function mint(uint256 tokenId) external virtual only(DEFAULT_ADMIN_ROLE) {
         _mint(_msgSender(), tokenId);
     }
 
-    function setTokenURI(uint256 tokenId, string memory tokenURI) internal virtual {
+    function setTokenURI(uint256 tokenId, string calldata tokenURI) external virtual only(DEFAULT_ADMIN_ROLE) {
         _setTokenURI(tokenId, tokenURI);
     }
 
-    function getMetaData(uint256 tokenId) external view override returns(bytes memory) {
+    /**
+     * @notice returns bytes metadata for token
+     * @dev metadata is made of only tokenURI for example but it can be any arbitrary bytes
+     * @param tokenId tokenId to fetch metadata
+     */
+    function getMetaData(uint256 tokenId) external view override virtual returns(bytes memory) {
         return abi.encode(tokenURI(tokenId));
     }
 
-    function setMetaData(uint256 tokenId, bytes calldata metadata) external override {
+    /**
+     * @notice set metadata for token, callable only by MetaDataERC721Predicate
+     * @dev metadata is made of only tokenURI for example but it can be any arbitrary bytes
+     * @param tokenId tokenId to fetch metadata
+     * @param metadata bytes data that can be decoded updated for token
+     */
+    function setMetaData(uint256 tokenId, bytes calldata metadata) external override virtual only(PREDICATE_ROLE) {
         string memory tokenURI = abi.decode(metadata, (string));
         _setTokenURI(tokenId, tokenURI);
     }
--- contracts/root/RootToken/IMetaDataERC721.sol
+++ contracts/root/RootToken/IMetaDataERC721.sol
@@ -2,9 +2,19 @@ import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
 pragma solidity ^0.6.6;
 
-// todo: comments
 interface IMetaDataERC721 is IERC721 {
+    /**
+     * @notice returns bytes metadata for token that is transferred
+     * while depositing token to matic chain
+     * @param tokenId tokenId to fetch metadata
+     */
     function getMetaData(uint256 tokenId) external view returns(bytes memory);
 
+    /**
+     * @notice set metadata for token, callable only by MetaDataERC721Predicate
+     * MetaData is set while withdrawing token from matic chain
+     * @param tokenId tokenId to fetch metadata
+     * @param metadata bytes data that can be decoded updated for token
+     */
     function setMetaData(uint256 tokenId, bytes calldata metadata) external;
 }
