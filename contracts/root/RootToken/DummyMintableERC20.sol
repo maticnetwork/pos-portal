25f3249c23081315a1d264f85f9a3afdfeba7cf0
Anjan Roy <anjanroy@yandex.com>
2020-12-17 15:31:50 +0530

    new: dummy mintable erc20 contract, can be deployed on root chain

---
+++ contracts/root/RootToken/DummyMintableERC20.sol
@@ -0,0 +1,45 @@
+pragma solidity 0.6.6;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {IMintableERC20} from "./IMintableERC20.sol";
+import {NativeMetaTransaction} from "../../common/NativeMetaTransaction.sol";
+import {ContextMixin} from "../../common/ContextMixin.sol";
+import {AccessControlMixin} from "../../common/AccessControlMixin.sol";
+
+contract DummyMintableERC20 is
+    ERC20,
+    AccessControlMixin,
+    NativeMetaTransaction,
+    ContextMixin,
+    IMintableERC20
+{
+    bytes32 public constant PREDICATE_ROLE = keccak256("PREDICATE_ROLE");
+
+    constructor(string memory name_, string memory symbol_)
+        public
+        ERC20(name_, symbol_)
+    {
+        _setupContractId("DummyMintableERC20");
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+        _setupRole(PREDICATE_ROLE, _msgSender());
+
+        _mint(_msgSender(), 10**10 * (10**18));
+        _initializeEIP712(name_);
+    }
+
+    /**
+     * @dev See {IMintableERC20-mint}.
+     */
+    function mint(address user, uint256 amount) external override only(PREDICATE_ROLE) {
+        _mint(user, amount);
+    }
+
+    function _msgSender()
+        internal
+        override
+        view
+        returns (address payable sender)
+    {
+        return ContextMixin.msgSender();
+    }
+}
