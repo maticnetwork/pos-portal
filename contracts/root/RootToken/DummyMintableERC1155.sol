8fe3beda817b4061a6171a4dee6d89a236cb2f74
Anjan Roy <anjanroy@yandex.com>
2020-12-18 17:59:15 +0530

    new: dummy mintable erc1155

---
+++ contracts/root/RootToken/DummyMintableERC1155.sol
@@ -0,0 +1,52 @@
+pragma solidity 0.6.6;
+
+import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import {IMintableERC1155} from "./IMintableERC1155.sol";
+import {NativeMetaTransaction} from "../../common/NativeMetaTransaction.sol";
+import {ContextMixin} from "../../common/ContextMixin.sol";
+import {AccessControlMixin} from "../../common/AccessControlMixin.sol";
+
+contract DummyMintableERC1155 is
+    ERC1155,
+    AccessControlMixin,
+    NativeMetaTransaction,
+    ContextMixin,
+    IMintableERC1155
+{
+    bytes32 public constant PREDICATE_ROLE = keccak256("PREDICATE_ROLE");
+
+    constructor(string memory uri_) public ERC1155(uri_) {
+        _setupContractId("DummyMintableERC1155");
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+        _setupRole(PREDICATE_ROLE, _msgSender());
+
+        _initializeEIP712(uri_);
+    }
+
+    function mint(
+        address account,
+        uint256 id,
+        uint256 amount,
+        bytes memory data
+    ) external override only(PREDICATE_ROLE) {
+        _mint(account, id, amount, data);
+    }
+
+    function mintBatch(
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) external override only(PREDICATE_ROLE) {
+        _mintBatch(to, ids, amounts, data);
+    }
+
+    function _msgSender()
+        internal
+        override
+        view
+        returns (address payable sender)
+    {
+        return ContextMixin.msgSender();
+    }
+}
