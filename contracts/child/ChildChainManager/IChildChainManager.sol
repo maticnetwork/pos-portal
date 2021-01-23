d712afbc9a48054e2a0d6351a54299589e19c148
Jaynti Kanani <jdkanani@gmail.com>
2021-01-23 17:05:56 +0530

    chg: add cleanup mapping tokens

--- contracts/child/ChildChainManager/IChildChainManager.sol
+++ contracts/child/ChildChainManager/IChildChainManager.sol
@@ -4,4 +4,5 @@ interface IChildChainManager {
     event TokenMapped(address indexed rootToken, address indexed childToken);
 
     function mapToken(address rootToken, address childToken) external;
+    function cleanMapToken(address rootToken, address childToken) external;
 }
