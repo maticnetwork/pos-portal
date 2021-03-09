3b79ef6060e6bf03b135e9de5c11817b5c2da81f
Anjan Roy <anjanroy@yandex.com>
2021-03-09 16:21:32 +0530

    new: batch deposit/ withdraw in child mintable ERC721 implementation

--- contracts/child/ChildToken/ChildMintableERC721.sol
+++ contracts/child/ChildToken/ChildMintableERC721.sol
@@ -19,6 +19,14 @@ contract ChildMintableERC721 is
 
     event TransferWithMetadata(address indexed from, address indexed to, uint256 indexed tokenId, bytes metaData);
 
+    // limit batching of tokens due to gas limit restrictions
+    uint256 public constant BATCH_LIMIT = 20;
+
+    event WithdrawnBatch(
+        address indexed user,
+        uint256[] tokenIds
+    );
+
     constructor(
         string memory name_,
         string memory symbol_,
@@ -44,20 +52,34 @@ contract ChildMintableERC721 is
     /**
      * @notice called when token is deposited on root chain
      * @dev Should be callable only by ChildChainManager
-     * Should handle deposit by minting the required tokenId for user
+     * Should handle deposit by minting the required tokenId(s) for user
      * Should set `withdrawnTokens` mapping to `false` for the tokenId being deposited
      * Minting can also be done by other functions
      * @param user user address for whom deposit is being done
-     * @param depositData abi encoded tokenId
+     * @param depositData abi encoded tokenIds. Batch deposit also supported.
      */
     function deposit(address user, bytes calldata depositData)
         external
         override
         only(DEPOSITOR_ROLE)
     {
-        uint256 tokenId = abi.decode(depositData, (uint256));
-        withdrawnTokens[tokenId] = false;
-        _mint(user, tokenId);
+
+        // deposit single
+        if (depositData.length == 32) {
+            uint256 tokenId = abi.decode(depositData, (uint256));
+            withdrawnTokens[tokenId] = false;
+            _mint(user, tokenId);
+
+        // deposit batch
+        } else {
+            uint256[] memory tokenIds = abi.decode(depositData, (uint256[]));
+            uint256 length = tokenIds.length;
+            for (uint256 i; i < length; i++) {
+                withdrawnTokens[tokenIds[i]] = false;
+                _mint(user, tokenIds[i]);
+            }
+        }
+
     }
 
     /**
@@ -71,6 +93,35 @@ contract ChildMintableERC721 is
         require(_msgSender() == ownerOf(tokenId), "ChildMintableERC721: INVALID_TOKEN_OWNER");
         withdrawnTokens[tokenId] = true;
         _burn(tokenId);
+    }
+
+    /**
+     * @notice called when user wants to withdraw multiple tokens back to root chain
+     * @dev Should burn user's tokens. This transaction will be verified when exiting on root chain
+     * @param tokenIds tokenId list to withdraw
+     */
+    function withdrawBatch(uint256[] calldata tokenIds) external {
+
+        uint256 length = tokenIds.length;
+        require(length <= BATCH_LIMIT, "ChildMintableERC721: EXCEEDS_BATCH_LIMIT");
+
+        // Iteratively burn ERC721 tokens, for performing
+        // batch withdraw
+        for (uint256 i; i < length; i++) {
+
+            uint256 tokenId = tokenIds[i];
+
+            require(_msgSender() == ownerOf(tokenId), string(abi.encodePacked("ChildMintableERC721: INVALID_TOKEN_OWNER ", tokenId)));
+            withdrawnTokens[tokenId] = true;
+            _burn(tokenId);
+
+        }
+
+        // At last emit this event, which will be used
+        // in MintableERC721 predicate contract on L1
+        // while verifying burn proof
+        emit WithdrawnBatch(_msgSender(), tokenIds);
+
     }
 
     /**
