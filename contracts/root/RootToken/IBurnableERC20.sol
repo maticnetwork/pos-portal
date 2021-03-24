90f70315bbd28e4abfde6c6725e3b6930c346578
Anjan Roy <anjanroy@yandex.com>
2021-03-24 11:28:55 +0530

    new: dummy burnable ERC20 L1 contract

--- contracts/root/RootToken/IBurnableERC20.sol
+++ contracts/root/RootToken/IBurnableERC20.sol
@@ -7,9 +7,9 @@ interface IBurnableERC20 is IERC20 {
      * @notice called by predicate contract to burn tokens while withdrawing, not transferring to withdrawer
      * @dev Should be callable only by BurnableERC20Predicate
      *
-     * It'll be better to do burning only here, so that when certain event signature
-     * is seen while unlocking tokens, tokens are burnt on L1, as they were done on L2
-     * when `withdraw` method was invoked in respective child contract.
+     * When certain event signature is seen while unlocking tokens, 
+     * they are burnt on L1, as they were on L2, when `withdraw` method 
+     * was invoked in respective child contract.
      *
      * @param user user address for whom token is being minted
      * @param amount amount of token being minted
