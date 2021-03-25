59068e2d02450ea6538401e806a0a8cd976c6395
Anjan Roy <anjanroy@yandex.com>
2021-03-25 12:30:24 +0530

    new: removed redundant functions

--- contracts/root/TokenPredicates/BurnableERC1155Predicate.sol
+++ contracts/root/TokenPredicates/BurnableERC1155Predicate.sol
@@ -107,67 +107,6 @@ contract MintableERC1155Predicate is
     }
 
     /**
-     * @notice Creates an array of `size` by repeating provided address,
-     * to be required for passing to batch balance checking function of ERC1155 tokens.
-     * @param addr Address to be repeated `size` times in resulting array
-     * @param size Size of resulting array
-     */
-    function makeArrayWithAddress(address addr, uint256 size)
-        internal
-        pure
-        returns (address[] memory)
-    {
-        require(
-            addr != address(0),
-            "BurnableERC1155Predicate: Invalid address"
-        );
-        require(
-            size > 0,
-            "BurnableERC1155Predicate: Invalid resulting array length"
-        );
-
-        address[] memory addresses = new address[](size);
-
-        for (uint256 i = 0; i < size; i++) {
-            addresses[i] = addr;
-        }
-
-        return addresses;
-    }
-
-    /**
-     * @notice Calculates amount of tokens to be minted, by subtracting available
-     * token balances from amount of tokens to be exited
-     * @param tokenBalances Token balances this contract holds for some ordered token ids
-     * @param amountsToBeExited Amount of tokens being exited
-     */
-    function calculateAmountsToBeMinted(
-        uint256[] memory tokenBalances,
-        uint256[] memory amountsToBeExited
-    ) internal pure returns (uint256[] memory) {
-        require(
-            tokenBalances.length == amountsToBeExited.length,
-            "BurnableERC1155Predicate: Array length mismatch found"
-        );
-
-        uint256[] memory toBeMintedAmounts = new uint256[](
-            tokenBalances.length
-        );
-
-        // Iteratively calculating amounts of token to be minted
-        //
-        // Please note, in some cases it can be 0, but that will not
-        // be a problem, due to implementation of mint logic for ERC1155
-        for (uint256 i = 0; i < tokenBalances.length; i++) {
-            if (tokenBalances[i] < amountsToBeExited[i]) {
-                toBeMintedAmounts[i] = amountsToBeExited[i] - tokenBalances[i];
-            }
-        }
-
-        return toBeMintedAmounts;
-    }
-
-    /**
      * @notice Validates log signature, from and to address
      * then sends the correct tokenId, amount to withdrawer
      * callable only by manager
