449d4abb10ec05ce95c0ae10631df36e3d39fc19
Anjan Roy <anjanroy@yandex.com>
2021-03-26 11:03:08 +0530

    new: burnable erc1155 dummy root token

---
+++ contracts/root/RootToken/DummyBurnableERC1155.sol
@@ -0,0 +1,64 @@
+pragma solidity 0.6.6;
+
+import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import {IBurnableERC1155} from "./IBurnableERC1155.sol";
+import {NativeMetaTransaction} from "../../common/NativeMetaTransaction.sol";
+import {ContextMixin} from "../../common/ContextMixin.sol";
+import {AccessControlMixin} from "../../common/AccessControlMixin.sol";
+
+contract DummyBurnableERC1155 is
+    ERC1155,
+    AccessControlMixin,
+    NativeMetaTransaction,
+    ContextMixin,
+    IBurnableERC1155
+{
+    bytes32 public constant PREDICATE_ROLE = keccak256("PREDICATE_ROLE");
+
+    constructor(string memory uri_) public ERC1155(uri_) {
+        _setupContractId("DummyBurnableERC1155");
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+        _setupRole(PREDICATE_ROLE, _msgSender());
+
+        _initializeEIP712(uri_);
+    }
+
+    function burn(
+        address account,
+        uint256 id,
+        uint256 amount,
+        bytes calldata data
+    ) external override only(PREDICATE_ROLE) {
+
+        if(data.length > 0) {
+            // do something interesting with data you received from L2
+        }
+
+        _burn(account, id, amount);
+
+    }
+
+    function burnBatch(
+        address account,
+        uint256[] calldata ids,
+        uint256[] calldata amounts,
+        bytes calldata data
+    ) external override only(PREDICATE_ROLE) {
+
+        if(data.length > 0) {
+            // do something interesting with data you received from L2
+        }
+
+        _burnBatch(account, ids, amounts);
+
+    }
+
+    function _msgSender()
+        internal
+        override
+        view
+        returns (address payable sender)
+    {
+        return ContextMixin.msgSender();
+    }
+}
