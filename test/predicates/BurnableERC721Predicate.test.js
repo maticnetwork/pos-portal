f87fd2345a8d24141588212f08ec578667304eae
Anjan Roy <anjanroy@yandex.com>
2021-03-31 12:43:55 +0530

    new: batch deposit [ BERC721Predicate ]

--- test/predicates/BurnableERC721Predicate.test.js
+++ test/predicates/BurnableERC721Predicate.test.js
@@ -96,4 +96,111 @@ contract('BurnableERC721Predicate', (accounts) => {
         })
     })
 
+    describe('lockTokens with batch deposit', () => {
+        const tokenId1 = mockValues.numbers[2]
+        const tokenId2 = mockValues.numbers[6]
+        const tokenId3 = mockValues.numbers[9]
+        const depositReceiver = mockValues.addresses[7]
+        const depositor = accounts[1]
+
+        let dummyBurnableERC721
+        let burnableERC721Predicate
+        let lockTokensTx
+        let lockedLog
+
+        before(async () => {
+            const contracts = await deployer.deployFreshRootContracts(accounts)
+
+            dummyBurnableERC721 = contracts.dummyBurnableERC721
+            burnableERC721Predicate = contracts.burnableERC721Predicate
+
+            await dummyBurnableERC721.mint(tokenId1, { from: depositor })
+            await dummyBurnableERC721.mint(tokenId2, { from: depositor })
+            await dummyBurnableERC721.mint(tokenId3, { from: depositor })
+
+            await dummyBurnableERC721.setApprovalForAll(burnableERC721Predicate.address, true, { from: depositor })
+        })
+
+        it('Depositor should have token', async () => {
+            {
+                const owner = await dummyBurnableERC721.ownerOf(tokenId1)
+                owner.should.equal(depositor)
+            }
+            {
+                const owner = await dummyBurnableERC721.ownerOf(tokenId2)
+                owner.should.equal(depositor)
+            }
+            {
+                const owner = await dummyBurnableERC721.ownerOf(tokenId3)
+                owner.should.equal(depositor)
+            }
+        })
+
+        it('Depositor should have approved token transfer', async () => {
+            const approved = await dummyBurnableERC721.isApprovedForAll(depositor, burnableERC721Predicate.address)
+            approved.should.equal(true)
+        })
+
+        it('Should be able to receive lockTokens tx', async () => {
+            const depositData = abi.encode(
+                ['uint256[]'],
+                [
+                    [tokenId1.toString(), tokenId2.toString(), tokenId3.toString()]
+                ]
+            )
+
+            lockTokensTx = await burnableERC721Predicate.lockTokens(depositor, depositReceiver, dummyBurnableERC721.address, depositData)
+            should.exist(lockTokensTx)
+        })
+
+        it('Should emit LockedBurnableERC721Batch log', () => {
+            const logs = logDecoder.decodeLogs(lockTokensTx.receipt.rawLogs)
+            lockedLog = logs.find(l => l.event === 'LockedBurnableERC721Batch')
+            should.exist(lockedLog)
+        })
+
+        describe('Correct values should be emitted in LockedBurnableERC721Batch log', () => {
+            it('Event should be emitted by correct contract', () => {
+                lockedLog.address.should.equal(
+                    burnableERC721Predicate.address.toLowerCase()
+                )
+            })
+
+            it('Should emit proper depositor', () => {
+                lockedLog.args.depositor.should.equal(depositor)
+            })
+
+            it('Should emit correct deposit receiver', () => {
+                lockedLog.args.depositReceiver.should.equal(depositReceiver)
+            })
+
+            it('Should emit correct root token', () => {
+                lockedLog.args.rootToken.should.equal(dummyBurnableERC721.address)
+            })
+
+            it('Should emit correct tokenIds', () => {
+                const lockedLogTokenIds = lockedLog.args.tokenIds.map(t => t.toNumber())
+
+                lockedLogTokenIds.should.include(tokenId1)
+                lockedLogTokenIds.should.include(tokenId2)
+                lockedLogTokenIds.should.include(tokenId3)
+            })
+        })
+
+        it('token should be transferred to correct contract', async () => {
+            {
+                const owner = await dummyBurnableERC721.ownerOf(tokenId1)
+                owner.should.equal(burnableERC721Predicate.address)
+            }
+            {
+                const owner = await dummyBurnableERC721.ownerOf(tokenId2)
+                owner.should.equal(burnableERC721Predicate.address)
+            }
+            {
+                const owner = await dummyBurnableERC721.ownerOf(tokenId3)
+                owner.should.equal(burnableERC721Predicate.address)
+            }
+        })
+    })
+
 })
