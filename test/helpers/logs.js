f81ecad4fa9e0b21ece608d17d6b37b831bfc556
Anjan Roy <anjanroy@yandex.com>
2021-04-06 11:40:36 +0530

    new: generate burn event log for BERC1155Predicate

--- test/helpers/logs.js
+++ test/helpers/logs.js
@@ -11,7 +11,13 @@ import {
   erc721BatchBurnEventSig,
   erc721BurnWithMetadataEventSig,
   erc1155TransferSingleEventSig,
-  erc1155TransferBatchEventSig
+  erc1155TransferBatchEventSig,
+  erc1155TransferSingleWithMetadataEventSig,
+  erc1155TransferBatchWithMetadataEventSig,
+  erc1155BurnSingleEventSig,
+  erc1155BurnBatchEventSig,
+  erc1155BurnSingleWithMetadataEventSig,
+  erc1155BurnBatchWithMetadataEventSig
 } from './constants'
 
 const abi = new AbiCoder()
@@ -204,6 +210,153 @@ export const getERC1155TransferBatchLog = ({
       [
         tokenIds.map(t => '0x' + t.toString(16)),
         amounts.map(a => '0x' + a.toString(16))
+      ]
+    )
+  ])
+}
+
+export const getERC1155TransferSingleWithMetadataLog = ({
+  overrideSig,
+  operator,
+  from,
+  to,
+  tokenId,
+  amount,
+  metaData
+}) => {
+  return RLP.encode([
+    '0x0',
+    [
+      overrideSig || erc1155TransferSingleWithMetadataEventSig,
+      operator,
+      from,
+      to
+    ],
+    abi.encode(['uint256', 'uint256', 'bytes'], ['0x' + tokenId.toString(16), '0x' + amount.toString(16), abi.encode(['string'], [metaData])])
+  ])
+}
+
+export const getERC1155TransferBatchWithMetadataLog = ({
+  overrideSig,
+  operator,
+  from,
+  to,
+  tokenIds,
+  amounts,
+  metaData
+}) => {
+  return RLP.encode([
+    '0x0',
+    [
+      overrideSig || erc1155TransferBatchWithMetadataEventSig,
+      operator,
+      from,
+      to
+    ],
+    abi.encode(
+      [
+        'uint256[]',
+        'uint256[]',
+        'bytes'
+      ],
+      [
+        tokenIds.map(t => '0x' + t.toString(16)),
+        amounts.map(a => '0x' + a.toString(16)),
+        abi.encode(['string'], [metaData])
+      ]
+    )
+  ])
+}
+
+export const getERC1155BurnSingleLog = ({
+  overrideSig,
+  operator,
+  from,
+  tokenId,
+  amount
+}) => {
+  return RLP.encode([
+    '0x0',
+    [
+      overrideSig || erc1155BurnSingleEventSig,
+      operator,
+      from
+    ],
+    abi.encode(['uint256', 'uint256'], ['0x' + tokenId.toString(16), '0x' + amount.toString(16)])
+  ])
+}
+
+export const getERC1155BurnBatchLog = ({
+  overrideSig,
+  operator,
+  from,
+  tokenIds,
+  amounts
+}) => {
+  return RLP.encode([
+    '0x0',
+    [
+      overrideSig || erc1155BurnBatchEventSig,
+      operator,
+      from
+    ],
+    abi.encode(
+      [
+        'uint256[]',
+        'uint256[]'
+      ],
+      [
+        tokenIds.map(t => '0x' + t.toString(16)),
+        amounts.map(a => '0x' + a.toString(16))
+      ]
+    )
+  ])
+}
+
+export const getERC1155BurnSingleWithMetadataLog = ({
+  overrideSig,
+  operator,
+  from,
+  tokenId,
+  amount,
+  metaData
+}) => {
+  return RLP.encode([
+    '0x0',
+    [
+      overrideSig || erc1155BurnSingleWithMetadataEventSig,
+      operator,
+      from
+    ],
+    abi.encode(['uint256', 'uint256', 'bytes'], ['0x' + tokenId.toString(16), '0x' + amount.toString(16), abi.encode(['string'], [metaData])])
+  ])
+}
+
+export const getERC1155BurnBatchWithMetadataLog = ({
+  overrideSig,
+  operator,
+  from,
+  tokenIds,
+  amounts,
+  metaData
+}) => {
+  return RLP.encode([
+    '0x0',
+    [
+      overrideSig || erc1155BurnBatchWithMetadataEventSig,
+      operator,
+      from
+    ],
+    abi.encode(
+      [
+        'uint256[]',
+        'uint256[]',
+        'bytes'
+      ],
+      [
+        tokenIds.map(t => '0x' + t.toString(16)),
+        amounts.map(a => '0x' + a.toString(16)),
+        abi.encode(['string'], [metaData])
       ]
     )
   ])
