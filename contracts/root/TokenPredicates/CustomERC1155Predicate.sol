9f8ae360f59b4a902b387b91108c1fd0b88e5115
Anjan Roy <anjanroy@yandex.com>
2021-04-26 11:37:36 +0530

    new: deposit functionality in custom erc1155 predicate

---
+++ contracts/root/TokenPredicates/CustomERC1155Predicate.sol
@@ -0,0 +1,103 @@
+pragma solidity 0.6.6;
+
+import {IMintableERC1155} from "../RootToken/IMintableERC1155.sol";
+import {
+    ERC1155Receiver
+} from "@openzeppelin/contracts/token/ERC1155/ERC1155Receiver.sol";
+import {AccessControlMixin} from "../../common/AccessControlMixin.sol";
+import {RLPReader} from "../../lib/RLPReader.sol";
+import {ITokenPredicate} from "./ITokenPredicate.sol";
+import {Initializable} from "../../common/Initializable.sol";
+
+contract CustomERC1155Predicate is
+    ITokenPredicate,
+    ERC1155Receiver,
+    AccessControlMixin,
+    Initializable
+{
+    using RLPReader for bytes;
+    using RLPReader for RLPReader.RLPItem;
+
+    bytes32 public constant MANAGER_ROLE = keccak256("MANAGER_ROLE");
+    bytes32 public constant TOKEN_TYPE = keccak256("CustomERC1155");
+    bytes32 public constant CHAIN_EXIT_EVENT_SIG = keccak256("ChainExit(address,uint256,uint256,bytes)");
+
+    event LockedBatchCustomERC1155(
+        address indexed depositor,
+        address indexed depositReceiver,
+        address indexed rootToken,
+        uint256[] ids,
+        uint256[] amounts
+    );
+
+    constructor() public {}
+
+    function initialize(address _owner) external initializer {
+        _setupContractId("CustomERC1155Predicate");
+        _setupRole(DEFAULT_ADMIN_ROLE, _owner);
+        _setupRole(MANAGER_ROLE, _owner);
+    }
+
+    /**
+     * @notice rejects single transfer
+     */
+    function onERC1155Received(
+        address,
+        address,
+        uint256,
+        uint256,
+        bytes calldata
+    ) external override returns (bytes4) {
+        return 0;
+    }
+
+    /**
+     * @notice accepts batch transfer
+     */
+    function onERC1155BatchReceived(
+        address,
+        address,
+        uint256[] calldata,
+        uint256[] calldata,
+        bytes calldata
+    ) external override returns (bytes4) {
+        return ERC1155Receiver(0).onERC1155BatchReceived.selector;
+    }
+
+    /**
+     * @notice Lock ERC1155 tokens for deposit, callable only by manager
+     * @param depositor Address who wants to deposit tokens
+     * @param depositReceiver Address (address) who wants to receive tokens on child chain
+     * @param rootToken Token which gets deposited
+     * @param depositData ABI encoded id array and amount array
+     */
+    function lockTokens(
+        address depositor,
+        address depositReceiver,
+        address rootToken,
+        bytes calldata depositData
+    ) external override only(MANAGER_ROLE) {
+        // forcing batch deposit since supporting both single and batch deposit introduces too much complexity
+        (
+            uint256[] memory ids,
+            uint256[] memory amounts,
+            bytes memory data
+        ) = abi.decode(depositData, (uint256[], uint256[], bytes));
+
+        emit LockedBatchCustomERC1155(
+            depositor,
+            depositReceiver,
+            rootToken,
+            ids,
+            amounts
+        );
+        IMintableERC1155(rootToken).safeBatchTransferFrom(
+            depositor,
+            address(this),
+            ids,
+            amounts,
+            data
+        );
+    }
+
+}
