f82752ca276959f4e4e24878dc1a59bddf7c7df8
Anjan Roy <anjanroy@yandex.com>
2021-07-20 12:59:40 +0530

    fix: comment [ hal-03 ]

--- contracts/root/RootChainManager/RootChainManager.sol
+++ contracts/root/RootChainManager/RootChainManager.sol
@@ -140,7 +140,7 @@ contract RootChainManager is
     }
 
     /**
-     * @notice Register a token predicate address against its type, callable only by mappers
+     * @notice Register a token predicate address against its type, callable only by ADMIN
      * @dev A predicate is a contract responsible to process the token specific logic while locking or exiting tokens
      * @param tokenType bytes32 unique identifier for the token type
      * @param predicateAddress address of token predicate address
@@ -192,7 +192,7 @@ contract RootChainManager is
 
     /**
      * @notice Remap a token that has already been mapped, properly cleans up old mapping
-     * Callable only by mappers
+     * Callable only by ADMIN
      * @param rootToken address of token on root chain
      * @param childToken address of token on child chain
      * @param tokenType bytes32 unique identifier for the token type
