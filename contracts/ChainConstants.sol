bccc1be5d3d67ac4b3c84ce3d148290c470ad82a
Denis Ermolin <denis.ermolin@matic.network>
2021-07-26 16:13:49 +0700

    misc: remove and ignore generated file

--- contracts/ChainConstants.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity >=0.4.21 <0.7.0;
-
-contract ChainConstants {
-    uint256 constant public ROOT_CHAIN_ID = 5;
-    bytes constant public ROOT_CHAIN_ID_BYTES = hex"05";
-
-    uint256 constant public CHILD_CHAIN_ID = 15001;
-    bytes constant public CHILD_CHAIN_ID_BYTES = hex"3A99";
-}
